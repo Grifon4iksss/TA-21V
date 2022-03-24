5c874be6fd8271582e320485e5eaa127538dc19f
Grigori Amelin <grigori.amelin2@gmail.com>
2022-03-24 11:29:01 +0200

    Update Tictactoe.py

--- Tictactoe.py
+++ Tictactoe.py
@@ -5,42 +5,84 @@ if next_position == "1":
     print(". . .")
     print(". . .")
 
-if next_position == "2":
+    next_position = input("Type what position: ")
+
+    if next_position == "1":
+        print("CAN'T DO THE MOVE")
+
+    elif next_position == "2":
+        print("x o .")
+        print(". . .")
+        print(". . .")
+
+    elif next_position == "2" and "1":
+        print("CAN'T DO THE MOVE")
+
+    elif next_position == "3":
+        print("x . o")
+        print(". . .")
+        print(". . .")
+    elif next_position == "4":
+        print("x . .")
+        print("o . .")
+        print(". . .")
+    elif next_position == "5":
+        print("x . .")
+        print(". o .")
+        print(". . .")
+    elif next_position == "6":
+        print("x . .")
+        print(". . o")
+        print(". . .")
+    elif next_position == "7":
+        print("x . .")
+        print(". . .")
+        print("o . .")
+    elif next_position == "8":
+        print("x . .")
+        print(". . .")
+        print(". o .")
+    elif next_position == "9": 
+        print("x . .")
+        print(". . .")
+        print(". . o")
+
+elif next_position == "2":
     print(". x .")
     print(". . .")
     print(". . .")
 
-if next_position == "3":
+elif next_position == "3":
     print(". . x")
     print(". . .")
     print(". . .")
 
-if next_position == "4":
+elif next_position == "4":
     print(". . .")
     print("x . .")
     print(". . .")
 
-if next_position == "5":
+elif next_position == "5":
     print(". . .")
     print(". x .")
     print(". . .")
 
-if next_position == "6":
+elif next_position == "6":
     print(". . .")
     print(". . x")
     print(". . .")
 
-if next_position == "7":
+elif next_position == "7":
     print(". . .")
     print(". . .")
     print("x . .")
 
-if next_position == "8":
+elif next_position == "8":
     print(". . .")
     print(". . .")
     print(". x .")
 
-if next_position == "9":
+elif next_position == "9":
     print(". . .")
     print(". . .")
     print(". . x")
