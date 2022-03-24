3964d28564333180f170a16e75140ac1dbe70604
Grigori Amelin <grigori.amelin2@gmail.com>
2022-03-24 11:06:46 +0200

    Update Tictactoe.py

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,3 +1,46 @@
-print(".x.")
-print("o..")
-print("...")
+next_position = input("Type what position: ")
+
+if next_position == "1":
+    print("x . .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "2":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "3":
+    print(". . x")
+    print(". . .")
+    print(". . .")
+
+if next_position == "4":
+    print(". . .")
+    print("x . .")
+    print(". . .")
+
+if next_position == "5":
+    print(". . .")
+    print(". x .")
+    print(". . .")
+
+if next_position == "6":
+    print(". . .")
+    print(". . x")
+    print(". . .")
+
+if next_position == "7":
+    print(". . .")
+    print(". . .")
+    print("x . .")
+
+if next_position == "8":
+    print(". . .")
+    print(". . .")
+    print(". x .")
+
+if next_position == "9":
+    print(". . .")
+    print(". . .")
+    print(". . x")
